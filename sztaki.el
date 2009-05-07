6626384f2800673a5d01b18e5108f3d03f106a2a
Török Gábor <gabor@20y.hu>
2009-05-07 12:42:48 +0200

    Allow to to call function `sztaki-lookup-phrase` from a non-interactive context.

--- sztaki.el
+++ sztaki.el
@@ -5,19 +5,21 @@
 
 (require 'w3m)
 
-(defun sztaki-lookup-phrase ()
-  "Look up the phrase under cursor and echo responsed translation if any."
-  (interactive)
-  (let* ((phrase (downcase (thing-at-point 'word)))
-         (url (concat "http://szotar.sztaki.hu/dict_search.php"
-                      "?O=HUN&E=1&L=ENG%3AHUN%3AEngHunDict&in_emacs=1&W="
-                      (w3m-url-encode-string phrase)))
-         (match (format "phrase '%s' not found" phrase)))
+(defun sztaki-lookup-phrase (phrase)
+  "Look up the PHRASE and echo responsed translation if any.
+
+If called interactivly, look up word under the cursor."
+  (interactive
+   (list (downcase (thing-at-point 'word))))
+  (let ((url (concat "http://szotar.sztaki.hu/dict_search.php"
+                     "?O=HUN&E=1&L=ENG%3AHUN%3AEngHunDict&in_emacs=1&W="
+                     (w3m-url-encode-string phrase)))
+        (match (format "phrase '%s' not found" phrase)))
     (with-temp-buffer
       (w3m-process-with-wait-handler
         (w3m-retrieve-and-render url nil nil nil nil handler))
       (when (re-search-forward (concat phrase ":.*") nil t)
-        (setq match (match-string 0))))
-    (message (concat "Phrase to translate (ENG->HUN): " match))))
+        (setq match (match-string-no-properties 0))))
+    (message (concat "SZTAKI (ENG->HUN): " match))))
 
 (provide 'sztaki)
